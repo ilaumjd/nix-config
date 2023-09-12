67fda1c354964de2417e9403700754e5d6fd8c6d
ilaumjd <ilaumjd@gmail.com>
2023-09-12 07:37:34 +0700

    modularize brew

---
+++ brew.nix
@@ -0,0 +1,14 @@
+{
+    enable = true;
+    global.brewfile = true;
+    onActivation.cleanup = "uninstall";
+    caskArgs.no_quarantine = true;
+    casks = [
+      "brave-browser"
+      "firefox"
+      "iterm2"
+      "mac-mouse-fix"
+      "utm"
+      "raycast"
+    ];
+}
--- darwin.nix
+++ darwin.nix
@@ -45,19 +45,6 @@
   ]; }) ];
 
   # Homebrew
-  homebrew = {
-    enable = true;
-    global.brewfile = true;
-    onActivation.cleanup = "uninstall";
-    caskArgs.no_quarantine = true;
-    casks = [
-      "brave-browser"
-      "firefox"
-      "iterm2"
-      "mac-mouse-fix"
-      "utm"
-      "raycast"
-    ];
-  };
+  homebrew = import ./brew.nix;
 
 }
