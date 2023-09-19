8d3358114c97f9d421ddf8e6bd5148e07c1164df
ilaumjd <ilaumjd@gmail.com>
2023-09-19 07:11:23 +0700

    vscode configs

--- home/vscode.nix
+++ home/vscode.nix
@@ -5,5 +5,9 @@
   userSettings = {
     "git.autofetch" = true;
     "jest.autoRun" = "off";
+
+    "editor.fontSize" = 13;
+    "terminal.integrated.fontFamily" = "Hack Nerd Font";
+    "terminal.integrated.fontSize" = 13;
   };
 }
