e4f0b09c0c0a3005dd65075d7f5d0c265f436dc8
ilaumjd <ilaumjd@gmail.com>
2023-09-19 15:58:50 +0700

    git configs

--- home/git/git.nix
+++ home/git/git.nix
@@ -2,8 +2,15 @@
   enable = true;
   userName = "Ilham AM";
   userEmail = "aulmajid@gmail.com";
+  extraConfig = {
+    commit.gpgsign = true;
+    gpg.format = "ssh";
+    gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+    user.signingkey = "~/.ssh/id_ed25519.pub";
+  };
   ignores = [
     ".envrc"
+    ".direnv"
   ];
   delta.enable = true;
 }
