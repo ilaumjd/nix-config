63407b76f320694cd8928a4305b5334846ef4321
ilaumjd <ilaumjd@gmail.com>
2023-09-24 09:54:34 +0700

    neovim plugins

--- home/nvim/plugins.nix
+++ home/nvim/plugins.nix
@@ -1,9 +1,25 @@
 pkgs:
 
 with pkgs.vimPlugins; [
-  nvim-cmp
-  vim-airline
+  vim-tmux-navigator
+  vim-ReplaceWithRegister
+  nvim-web-devicons
 
+
+  ###### COMMANDS #####
+  {
+    plugin = comment-nvim;
+    type = "lua";
+    config = "require('Comment').setup()";
+  }
+  {
+    plugin = nvim-surround;
+    type = "lua";
+    config = "require('nvim-surround').setup()";
+  }
+
+
+  ###### LSP #####
   {
     plugin = lazy-lsp-nvim;
     type = "lua";
@@ -12,16 +28,44 @@ with pkgs.vimPlugins; [
         excluded_servers = {
           'sqls',
         },
+        preferred_servers = {
+          javascript = "eslint",
+          javascriptreact = "eslint",
+          typescript = "eslint",
+          typescriptreact = "eslint",
+          html = "tailwindcss",
+          nix = "rnix",
+        },
       }
     '';
   }
+
+
+  ###### FILE EXPLORER #####
   {
-    plugin = nvim-surround;
+    plugin = nvim-tree-lua;
     type = "lua";
-    config = "require('nvim-surround').setup()";
+    config = ''
+      require('nvim-tree').setup()
+      vim.g.loaded = 1
+      vim.g.loaded_netrwPlugin = 1
+    '';
   }
+  
+
+  ###### THEMES #####
   {
     plugin = sonokai;
     config = "colorscheme sonokai";
+  }
+  {
+    plugin = lualine-nvim;
+    type = "lua";
+    config = ''
+      local theme = require('lualine.themes.base16')
+      require('lualine').setup {
+        options = { theme = theme },
+      }
+    '';
   }
 ]
