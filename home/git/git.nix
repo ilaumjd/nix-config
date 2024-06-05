fe5661a9c6393a4bd72837373221249e5e2ec1ed
ilaumjd <ilaumjd@gmail.com>
2024-06-05 14:25:37 +0700

    git signing

--- home/git/git.nix
+++ home/git/git.nix
@@ -4,12 +4,12 @@
     userName = "ilaumjd";
     userEmail = "ilaumjd@gmail.com";
     lfs.enable = true;
-    # extraConfig = {
-    #   commit.gpgsign = true;
-    #   gpg.format = "ssh";
-    #   gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-    #   user.signingkey = "~/.ssh/id_ed25519.pub";
-    # };
+    extraConfig = {
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+    };
     ignores = [ ".envrc" ".direnv" ];
     delta.enable = true;
   };
