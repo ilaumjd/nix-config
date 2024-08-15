4f309a0e4c820679ba2ca3a9047798fbb3f3ed86
ilaumjd <ilaumjd@gmail.com>
2024-08-15 23:50:43 +0700

    setup android

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -41,6 +41,15 @@ pkgs: {
 
       # rust
       . "$HOME/.cargo/env"
+
+      # android
+      export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+      export PATH="$JAVA_HOME/bin:$PATH"
+      export ANDROID_HOME=$HOME/Library/Android/sdk
+      export PATH=$PATH:$ANDROID_HOME/emulator
+      export PATH=$PATH:$ANDROID_HOME/tools
+      export PATH=$PATH:$ANDROID_HOME/tools/bin
+      export PATH=$PATH:$ANDROID_HOME/platform-tools
     '';
   };
 }
