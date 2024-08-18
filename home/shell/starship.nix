458b3d01e03f96772e2b068d44b06cefe7f91e54
ilaumjd <ilaumjd@gmail.com>
2024-08-18 20:26:40 +0700

    configure starship

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -5,9 +5,42 @@
     enableZshIntegration = true;
     enableFishIntegration = true;
     settings = {
+      format = "$directory$character";
+      right_format = "$git_branch$git_status";
+      add_newline = false;
+      directory = {
+        style = "bold cyan";
+        truncation_length = 3;
+        truncate_to_repo = false;
+      };
+      git_branch = {
+        format = "[$symbol$branch]($style) ";
+        style = "bold purple";
+        symbol = " ";
+      };
+      git_status = {
+        format = "([\\[$all_status$ahead_behind\\]]($style))";
+        style = "bold red";
+        conflicted = "=";
+        ahead = "⇡\${count}";
+        behind = "⇣\${count}";
+        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+        untracked = "?";
+        stashed = "\$";
+        modified = "!";
+        staged = "+";
+        renamed = "»";
+        deleted = "✘";
+      };
       character = {
         success_symbol = "[➤](bold green)";
         error_symbol = "[➤](bold red)";
+        vimcmd_symbol = "[❮](bold green)";
+      };
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "yellow";
+        min_time = 500;
       };
     };
   };
