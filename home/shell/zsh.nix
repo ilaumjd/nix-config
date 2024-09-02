7a93513b5eea6ae98f13accbaa153568bbf6fccf
ilaumjd <ilaumjd@gmail.com>
2024-09-03 06:55:41 +0700

    android build-tools

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -24,9 +24,8 @@ pkgs: {
       export PATH="$JAVA_HOME/bin:$PATH"
       export ANDROID_HOME=$HOME/Library/Android/sdk
       export PATH=$PATH:$ANDROID_HOME/emulator
-      export PATH=$PATH:$ANDROID_HOME/tools
-      export PATH=$PATH:$ANDROID_HOME/tools/bin
       export PATH=$PATH:$ANDROID_HOME/platform-tools
+      export PATH=$PATH:$ANDROID_HOME/build-tools/35.0.0
     '';
   };
 }
