1d51211697a3e9917636cf16fc6311636bf6a2c7
ilaumjd <ilaumjd@gmail.com>
2024-09-04 09:19:24 +0700

    merge darwin default.nix to flake.nix

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -22,15 +22,43 @@
       env = import ../env.nix;
     in
     {
-      # Darwin
-      darwinConfigurations = import ./. {
-        inherit
-          env
-          nixpkgs
-          nix-darwin
-          home-manager
-          ;
-      };
+      darwinConfigurations = {
+        ${env.hostname} = nix-darwin.lib.darwinSystem {
+          specialArgs = {
+            inherit env;
+          };
+          modules = [
 
+            # Darwin
+            ./configuration.nix
+
+            # Nix Settings
+            (import ../nix-settings.nix)
+
+            # Nixpkgs
+            {
+              nixpkgs.pkgs = import nixpkgs {
+                system = env.system;
+                config.allowUnfree = true;
+                config.allowUnfreePredicate = pkgs: true;
+              };
+            }
+
+            # Home Manager
+            home-manager.darwinModules.home-manager
+            {
+              home-manager = {
+                backupFileExtension = "backup";
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.${env.user}.imports = [
+                  ../home
+                  { targets.darwin.currentHostDefaults."com.apple.controlcenter".BatteryShowPercentage = true; }
+                ];
+              };
+            }
+          ];
+        };
+      };
     };
 }
