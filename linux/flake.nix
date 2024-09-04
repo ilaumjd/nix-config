dbc8bae86cadd13d891132590ba900f790c45067
ilaumjd <ilaumjd@gmail.com>
2024-09-04 09:33:19 +0700

    merge linux standalone.nix to flake.nix

--- linux/flake.nix
+++ linux/flake.nix
@@ -18,16 +18,38 @@
     }:
     let
       env = import ../env.nix;
+      pkgs = import nixpkgs {
+        system = env.system;
+        config.allowUnfree = true;
+        config.allowUnfreePredicate = pkgs: true;
+        overlays = [ nixgl.overlay ];
+      };
+      nixGLWrap = import ./nixgl.nix pkgs;
     in
     {
 
-      homeConfigurations."${env.user}" = import ./standalone.nix {
-        inherit
-          env
-          nixpkgs
-          home-manager
-          nixgl
-          ;
+      homeConfigurations."${env.user}" = home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgs;
+        modules = [
+
+          # Home Manager
+          ../home
+
+          # Home Configs
+          {
+            home.username = env.user;
+            home.homeDirectory = "/home/${env.user}";
+          }
+
+          # Nix Settings
+          (import ../nix-settings.nix)
+
+          {
+            nix.package = pkgs.nixVersions.git;
+            programs.kitty.package = nixGLWrap pkgs.kitty;
+            programs.wezterm.package = nixGLWrap pkgs.wezterm;
+          }
+        ];
       };
 
     };
