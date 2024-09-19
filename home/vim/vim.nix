67ffebbc0cbef8da17410f18a72126cf1e7ceec8
ilaumjd <ilaumjd@gmail.com>
2024-09-19 08:20:26 +0700

    configure vim

--- home/vim/vim.nix
+++ home/vim/vim.nix
@@ -1,14 +1,16 @@
 pkgs: {
   programs.vim = {
+    packageConfigurable = if pkgs.stdenv.isDarwin then pkgs.vim-darwin else pkgs.vim-full;
     enable = true;
     plugins = with pkgs.vimPlugins; [
+      auto-pairs
       ctrlp-vim
       nord-vim
       vim-airline
       vim-commentary
       vim-gitgutter
       vim-nix
-      ReplaceWithRegister
+      vim-subversive
     ];
     extraConfig = builtins.readFile ./vimrc;
   };
