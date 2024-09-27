0211fd0a731cd8f0dfd880aaf308f0d4a3375d46
ilaumjd <ilaumjd@gmail.com>
2024-09-27 20:18:21 +0700

    add cachix

--- nix-settings.nix
+++ nix-settings.nix
@@ -3,6 +3,12 @@ pkgs: {
     settings = {
       auto-optimise-store = true;
       experimental-features = "nix-command flakes";
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     extraOptions = ''
       keep-outputs = true
