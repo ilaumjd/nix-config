b1ea2a32ff320cd5128c7f4f24df1eb99d20fb0d
ilaumjd <ilaumjd@gmail.com>
2024-11-09 09:37:37 +0700

    custom rc zsh

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -15,35 +15,39 @@ pkgs: {
       }
     ];
     initExtra =
-      if pkgs.stdenv.isDarwin then
-        ''
-          # fish-like line navigation binding
-          bindkey '^[[1;9D' beginning-of-line
-          bindkey '^[[1;9C' end-of-line
-          bindkey '^[[1;2D' beginning-of-line
-          bindkey '^[[1;2C' end-of-line
+      (
+        if pkgs.stdenv.isDarwin then
+          ''
+            # fish-like line navigation binding
+            bindkey '^[[1;9D' beginning-of-line
+            bindkey '^[[1;9C' end-of-line
+            bindkey '^[[1;2D' beginning-of-line
+            bindkey '^[[1;2C' end-of-line
 
-          # android
-          export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
-          export PATH=$PATH:$JAVA_HOME/bin
-          export ANDROID_HOME=$HOME/Library/Android/sdk
-          export PATH=$PATH:$ANDROID_HOME/emulator
-          export PATH=$PATH:$ANDROID_HOME/platform-tools
-          export PATH=$PATH:$ANDROID_HOME/build-tools/35.0.0
+            # android
+            export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+            export PATH=$PATH:$JAVA_HOME/bin
+            export ANDROID_HOME=$HOME/Library/Android/sdk
+            export PATH=$PATH:$ANDROID_HOME/emulator
+            export PATH=$PATH:$ANDROID_HOME/platform-tools
+            export PATH=$PATH:$ANDROID_HOME/build-tools/35.0.0
+          ''
+        else
+          ''
+            # fish-like line navigation binding
+            bindkey '^[[1;5D' beginning-of-line
+            bindkey '^[[1;5C' end-of-line
+            bindkey '^[[1;2D' beginning-of-line
+            bindkey '^[[1;2C' end-of-line
 
-          # custom rc
-          export PATH=$PATH:$HOME/bin
-          source $HOME/.shrc
-        ''
-      else
-        ''
-          # fish-like line navigation binding
-          bindkey '^[[1;5D' beginning-of-line
-          bindkey '^[[1;5C' end-of-line
-          bindkey '^[[1;2D' beginning-of-line
-          bindkey '^[[1;2C' end-of-line
-
-          export PATH=$PATH:$HOME/swift/usr/bin
-        '';
+            # swift
+            export PATH=$PATH:$HOME/swift/usr/bin
+          ''
+      )
+      + ''
+        # custom rc
+        export PATH=$PATH:$HOME/bin
+        source $HOME/.shrc
+      '';
   };
 }
