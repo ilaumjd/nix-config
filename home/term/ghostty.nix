35515499f23f6f7336ee147ce7950736d1e20381
ilaumjd <ilaumjd@gmail.com>
2025-01-02 18:36:52 +0700

    add ghostty

---
+++ home/term/ghostty.nix
@@ -0,0 +1,10 @@
+config: {
+  programs.ghostty = {
+    enable = true;
+    enableBashIntegration = false;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    installBatSyntax = false;
+    installVimSyntax = true;
+  };
+}
