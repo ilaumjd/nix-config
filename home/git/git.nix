3a835e52d5096e4a28423520a9f92cb3dfc5f363
ilaumjd <ilaumjd@gmail.com>
2025-02-17 12:43:40 +0700

    git: update signing

--- home/git/git.nix
+++ home/git/git.nix
@@ -4,11 +4,13 @@
     userName = "ilaumjd";
     userEmail = "ilaumjd@gmail.com";
     lfs.enable = true;
+    signing = {
+      format = "ssh";
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
     extraConfig = {
-      commit.gpgsign = true;
-      gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-      user.signingkey = "~/.ssh/id_ed25519.pub";
     };
     ignores = [
       ".envrc"
